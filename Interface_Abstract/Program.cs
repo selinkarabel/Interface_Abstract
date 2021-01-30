c5a89a0a5df8ca4d606392bc7f9237468491c347
selinkarabel <selinkarabel@gmail.com>
2021-01-30 15:05:47 +0300

    Add project files.

---
+++ Interface_Abstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interface_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
